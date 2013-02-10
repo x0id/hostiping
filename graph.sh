458743b42b2223af4433987b6c578242e68886d8
Dmitriy Kargapolov <dmitriy.kargapolov@gmail.com>
2013-02-10 16:00:43 -0500

    initial commit

---
+++ graph.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# custom settings
+. `dirname $0`/env.sh
+
+h=${1:-24}
+u=${2:-90}
+let h=h*3600
+out=hstat.png
+
+cmd="rrdtool graph $out -s -$h"
+cmd="$cmd -w 1280 -h 770 -D"
+cmd="$cmd -v Seconds -u $u -r --grid-dash 1:3"
+cmd="$cmd HRULE:3#0000FF:'3 sec latency'"
+cmd="$cmd HRULE:30#00FF00:'30 sec latency'"
+cmd="$cmd HRULE:60#FF0000:'60 sec latency'"
+for id in "${!urls[@]}"; do
+    c=${color[$id]}
+    c=${c:-"#000000"}
+    cmd="$cmd DEF:$id=$rrd:$id:MAX LINE2:$id$c:$id"
+done
+
+eval $cmd
+
+if which feh 2>/dev/null; then
+    feh -ZF $out
+else
+    xdg-open $out
+fi
---
+++ hping.sh
@@ -0,0 +1,84 @@
+#!/bin/bash
+
+# current time in two formats
+t=`date -u +"%F_%T %s"`
+
+# custom settings
+. `dirname $0`/env.sh
+
+if [[ ! -d $dir ]]; then mkdir -p $dir; fi
+
+# format timestamp
+ts=`echo $t |awk '{print $1}'`
+
+# get utc seconds
+us=`echo $t |awk '{print $2}'`
+
+logerr() {
+    while read line; do
+        echo "`date -u --rfc-3339=ns` $line"
+    done
+}
+
+logtime() {
+    awk -v usec=$us -v id=$2 '
+    /real/  {
+        if (NF == 4 && $3 == "real")
+            print id, $4
+        else
+            exit 1
+    }' $1
+}
+
+poke() {
+    local id=$1
+    local out="$dir/${id}_$ts.out"
+    local err="$dir/${id}_$ts.err"
+    local url=${urls[$id]}
+    set -o pipefail
+    if (time -p wget -O $out $url >/dev/null) |& logerr >$err; then
+        # wc -l $out; cksum $out
+        if logtime $err $id; then
+            rm -f $out $err
+        fi
+    else
+        ret=$?
+        if [[ -e $out && ! -s $out ]]; then
+            # echo "removing empty file $out"
+            rm -f $out
+        fi
+        cat $err
+    fi
+}
+
+rrd_create() {
+    cmd="rrdtool create $1 -s 300"
+    for id in "${!urls[@]}"; do
+        cmd="$cmd DS:$id:GAUGE:600:U:U"
+    done
+    cmd="$cmd RRA:MAX:0.5:1:8928"  # 31 day of 5-min step
+    cmd="$cmd RRA:MAX:0.5:12:8784" # 366 days of 1-hour step
+    eval $cmd
+}
+
+if [[ ! -e $rrd ]]; then
+    rrd_create $rrd
+fi
+
+for id in "${!urls[@]}"; do
+    poke $id &
+done |awk -v usec=$us -v rrd=$rrd '
+BEGIN {
+    n = 0
+} {
+    key[n]   = $1
+    val[n++] = $2
+}
+END {
+    acc = "rrdtool update " rrd " -t " key[0]
+    for (i=1; i<n; i++) acc = acc ":" key[i]
+    acc = acc " -- " usec
+    for (i=0; i<n; i++) acc = acc ":" val[i]
+    system(acc)
+}
+'
